e693c156571a8568f812ab28b0a2f72d0aa5c35c
Giang Minh <giangndm@ows.vn>
2023-02-27 13:30:41 +0700

    106.0.5-fix2

---
+++ ios/RCTWebRTC/WebRTCModule+RTCPeerConnection.h
@@ -0,0 +1,17 @@
+#import "DataChannelWrapper.h"
+#import "WebRTCModule.h"
+#import <WebRTC/RTCPeerConnection.h>
+
+@interface RTCPeerConnection (React)
+
+@property (nonatomic, strong) NSNumber *reactTag;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, DataChannelWrapper *> *dataChannels;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, RTCMediaStream *> *remoteStreams;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, RTCMediaStreamTrack *> *remoteTracks;
+@property (nonatomic, weak) id webRTCModule;
+
+@end
+
+@interface WebRTCModule (RTCPeerConnection) <RTCPeerConnectionDelegate>
+
+@end
---
+++ tools/downloadBitcode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Bitcode is deprecated since Xcode 14."
+echo "This package no longer provides bitcode."
